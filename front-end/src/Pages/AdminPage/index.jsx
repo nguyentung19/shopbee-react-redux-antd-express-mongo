2557501370b8eb67c5f31d155fd8865da0e58f3d
nguyentung19 <nguyen.thanh.tung19051990@gmail.com>
2023-07-18 20:12:46 +0700

    no message

--- front-end/src/Pages/AdminPage/index.jsx
+++ front-end/src/Pages/AdminPage/index.jsx
@@ -2,9 +2,21 @@ import { Layout } from "antd";
 import Sider from "antd/es/layout/Sider";
 import { Content } from "antd/es/layout/layout";
 import SideMenu from "../../Components/AdminPage/SideMenu";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 export default function AdminPage() {
+  const navigate = useNavigate();
+  const currentUser = useSelector((state) => state.AuthAdmin.currentUser);
+
+  useEffect(() => {
+    if (currentUser || currentUser.role !== "admin") {
+      navigate("/");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentUser]);
+
   return (
     <Layout className="AdminPage">
       <Sider>
